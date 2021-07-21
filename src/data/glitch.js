cea05c2f04df2b1d33ffe9c3d70b6814891769c5
Daniel Scholtus <sdeonline@gmail.com>
2021-07-21 06:36:15 -0600

    Adds beliefs query

--- src/data/glitch.js
+++ src/data/glitch.js
@@ -4,15 +4,37 @@ const HOME = gql`
   query {
     doctrines {
       _id
+      order
       title
       beliefs {
         _id
+        order
         title
       }
     }
   }
 `
 
+const BELIEF = gql`
+  query($_id: ID!) {
+    belief(_id: $_id) {
+      _id
+      order
+      title
+      declarations {
+        _id
+        order
+        text
+        verses {
+          _id
+          reference
+        }
+      }
+    }
+  }
+`
+
 export default {
-    HOME
+    HOME,
+    BELIEF
 }
